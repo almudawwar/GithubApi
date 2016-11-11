b4b451b0c312be3324fba4e389f60de402f6f87f
rodrigopg8 <rodrigopg8@gmail.com>
2016-11-11 12:21:36 +0000

    Added the Volley library for network requests and defined the first layout for the activities.

--- app/src/main/java/com/example/rodrigo/githubapi/MainActivity.java
+++ app/src/main/java/com/example/rodrigo/githubapi/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.rodrigo.githubapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText search_text;
+    ListView result_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search_text = (EditText) findViewById(R.id.search_text);
+        result_list = (ListView) findViewById(R.id.result_list);
     }
 }
