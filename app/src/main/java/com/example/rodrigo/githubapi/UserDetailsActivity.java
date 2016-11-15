ef342769b4e80f2d213a710d3941351c9144dcdc
rodrigopg8 <rodrigopg8@gmail.com>
2016-11-15 14:20:56 +0000

    Showing the avatar of the user.

--- app/src/main/java/com/example/rodrigo/githubapi/UserDetailsActivity.java
+++ app/src/main/java/com/example/rodrigo/githubapi/UserDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.example.rodrigo.githubapi;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +11,8 @@ import android.widget.TextView;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -23,6 +24,8 @@ public class UserDetailsActivity extends AppCompatActivity {
     TextView username, stars, followers;
     Button back_button;
 
+    User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,31 +33,25 @@ public class UserDetailsActivity extends AppCompatActivity {
 
         user_url = getIntent().getStringExtra("user_url");
 
-        avatar = (ImageView) findViewById(R.id.avatar);
-        username = (TextView) findViewById(R.id.username);
-        stars = (TextView) findViewById(R.id.stars);
-        followers = (TextView) findViewById(R.id.followers);
-        back_button = (Button) findViewById(R.id.back_button);
+        setComponents();
 
-        back_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
-            }
-        });
+        setListeners();
 
         MySingleton.getInstance(this).addToRequestQueue(new JsonObjectRequest(Request.Method.GET, user_url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
 
                 try {
-                    User user = new User(response.getString("login"), response.getString("url"));
+                    user = new User(response.getString("login"), response.getString("url"));
 
                     user.setFollowers(response.getInt("followers"));
                     user.setAvatar(response.getString("avatar_url"));
 
                     username.setText(user.getLogin());
                     followers.setText(user.getFollowers() + "");
+
+                    makeAvatarRequest();
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -65,5 +62,44 @@ public class UserDetailsActivity extends AppCompatActivity {
 
             }
         }));
+    }
+
+    private void makeAvatarRequest(){
+
+        ImageRequest avatarRequest = new ImageRequest(user.getAvatar(), new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+
+                avatar.setImageBitmap(response);
+            }
+        }, 0, 0, null,
+                new Response.ErrorListener(){
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+
+        MySingleton.getInstance(UserDetailsActivity.this).addToRequestQueue(avatarRequest);
+    }
+
+    private void setComponents(){
+
+        avatar = (ImageView) findViewById(R.id.avatar);
+        username = (TextView) findViewById(R.id.username);
+        stars = (TextView) findViewById(R.id.stars);
+        followers = (TextView) findViewById(R.id.followers);
+        back_button = (Button) findViewById(R.id.back_button);
+    }
+
+    private void setListeners(){
+
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 }
